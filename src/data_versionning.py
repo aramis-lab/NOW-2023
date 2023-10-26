46f57dd6a148c286f59fcb3ff32b3177a9ebe3b3
NicolasGensollen <nicolas.gensollen@gmail.com>
2023-10-26 14:32:28 +0200

    add content to the data versioning notebook

--- src/data_versionning.py
+++ src/data_versionning.py
@@ -15,9 +15,17 @@
 # %% [markdown]
 # # Part 2 : Data versionning using DVC
 #
+# ## Chapter 1 : Getting started with DVC
+#
 # This is the second part of the tutorial on data versionning using [DVC](https://dvc.org).
 #
-# First, we need to install [DVC](https://dvc.org):
+# In this first chapter, we will install DVC and take a look at how DVC works with GIT in its most simple usage.
+#
+# ### Install DVC
+#
+# First, we need to install [DVC](https://dvc.org). There are various ways to install it depending on you OS, which you can browse [here](https://dvc.org/doc/install). For example, on MacOS, you can install it with `brew`, `conda`, or `pip`.
+#
+# If you are following this tutorial on your own machine, chose the option that makes the most sense. If you are following on the notebook, we will install DVC with `pip`:
 
 # %%
 # ! pip install dvc
@@ -31,12 +39,62 @@
 # %%
 # ! dvc --version
 
-# %%
-def g(x: str) -> str:
-    return x + x
+# %% [markdown]
+# ### Initialize a DVC repository
+#
+# Now that we have DVC installed we can start using it !
+#
+# First of all, it is very important to understand that DVC is not a replacement for GIT. It is a tool designed to work WITH GIT as it solves a different problem than GIT.
+#
+# In other words, you need both GIT and DVC to manage both code and data.
+#
+# To initialize a DVC repository, we need to be in a GIT-initialized repository, so let's do that:
 
+# %%
+# ! git init
+
+# %% [markdown]
+# You can check that a `.git` hidden folder was created:
 
 # %%
-g("test")
+# ! ls -lah
+
+# %% [markdown]
+# Now we can initialize the DVC repository:
+
+# %%
+# ! dvc init
+
+# %% [markdown]
+# In the exact same way as for `git init`, `dvc init` created a hidden folder named `.dvc`:
+
+# %%
+# ! ls -lah
+
+# %% [markdown]
+# In addition to this, DVC created a few files for us. To see that, we can use the `git status` command since we have a git repository:
+
+# %%
+# ! git status
+
+# %% [markdown]
+# As we can see, DVC created 2 files in the `.dvc` folder as well as one file in the current workspace :
+#
+# - `.dvc/.gitignore`
+# - `.dvc/config`
+# - `.dvcignore`
+#
+# These files need to be versionned with GIT, DVC already added them to the stagging aread, so all we need to do is commit them:
+
+# %%
+# ! git commit -m "initialize DVC"
+
+# %%
+# ! git log
+
+# %% [markdown]
+# And that's it, we have successfully initialized a DVC repository and we are now ready to track some code and data !
+#
+# ### Track code and data
 
 # %%
