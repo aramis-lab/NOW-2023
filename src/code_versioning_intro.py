a910fde3940225a221910c2828a977c0ae00f66e
camillebrianceau <camille.brianceau@gmail.com>
2023-11-14 14:53:15 +0100

    add tuto 1 for git

---
+++ src/code_versioning_intro.py
@@ -0,0 +1,16 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.15.2
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Chapter 1 : Introduction to code versioning
