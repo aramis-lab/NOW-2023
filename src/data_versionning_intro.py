76f5dfd308dc8ac644846aa144a9bfb0b29616f3
NicolasGensollen <nicolas.gensollen@gmail.com>
2023-11-15 10:59:46 +0100

    update to dvc intro

--- src/data_versionning_intro.py
+++ src/data_versionning_intro.py
@@ -20,16 +20,47 @@
 # %% [markdown]
 # ## What is data versioning ?
 #
-# TODO
+# Data Versioning is a technique to track changes made to data over time. It involves creating and storing different versions of data, allowing users to access and analyze specific versions whenever needed. Data Versioning ensures data consistency, traceability, and provides a historical record of changes made to datasets.
 #
+# - **Traceability:** Track and understand the changes made to the data over time. This is crucial for compliance, auditing, and debugging purposes.
+# - **Reproducibility:** By preserving previous versions of data, Data Versioning enables reproducibility of results. Users can analyze specific versions of the data used in previous analyses, ensuring consistency and accuracy of research and decision-making.
+# - **Collaboration:** Data Versioning facilitates collaboration among data analysts and data scientists. Multiple users can work on the same dataset simultaneously, knowing they can easily access and switch between different versions without affecting others' work.
+# - **Data Recovery:** In case of data corruption or accidental changes, Data Versioning provides a backup of previous versions, allowing businesses to recover and restore data to a specific point in time.
+
+# %% [markdown]
 # ## What are the tools ?
 #
-# TODO
+# In the area of code versioning, there are a few tools available but Git clearly dominates the area. When it comes to data versioning, a relatively younger field, there are still a lot of available tools. We won't focus on enumerating them all here, but we will name the most popular ones and give some pointers:
 #
+# - [Git-LFS](https://git-lfs.com) replaces large files with text pointers inside Git, while storing the file contents on a remote server.
+# - [Git-annex](https://git-annex.branchable.com) allows managing large files with git, without storing the file contents in git. It is a lower-level tool than [DataLad](https://www.datalad.org) and [DVC](https://dvc.org) which expose higher level abstractions to users.
+# - [DataLad](https://www.datalad.org) which depends on [Git-annex](https://git-annex.branchable.com). You can see it as a Python frontend to [Git-annex](https://git-annex.branchable.com). It provides a lot of useful abstractions to version datasets. The [DataLad handbook](https://handbook.datalad.org/en/latest/) is a great resource to learn how to use this tool.
+# - [DVC](https://dvc.org)## Why DVC ?
+#
+# In this tutorial we will rely on [DVC](https://dvc.org) rather than another tool from the list above. There are several reasons we made this choice:
+#
+# - Free and open source.
+# - Easy to install: DVC is written in Python and can be easily installed in various ways.
+# - The documentation is very good and easy to follow for newcommers.
+# - The commands are **very** similar to the git commands. This means that anybody used to work with git will be able to work with DVC in no time.
+#
+# These last points are particularly useful to enbark new contributors in a project relying on DVC for data management.
+#
+# That being said, you might be better using another tool. We strongly encourage you to take a closer look at them, especially [DataLad](https://www.datalad.org) which occupies a similar place in the versioning tools ecosystem.
+
+# %% [markdown]
 # ## Why DVC ?
 #
-# - easy to install
-# - dvc commands are very similar to the git commands
+# In this tutorial we will rely on [DVC](https://dvc.org) rather than another tool from the list above. There are several reasons we made this choice:
+#
+# - Free and open source.
+# - Easy to install: DVC is written in Python and can be easily installed in various ways.
+# - The documentation is very good and easy to follow for newcommers.
+# - The commands are **very** similar to the git commands. This means that anybody used to work with git will be able to work with DVC in no time.
+#
+# These last points are particularly useful to enbark new contributors in a project relying on DVC for data management.
+#
+# That being said, you might be better using another tool. We strongly encourage you to take a closer look at them, especially [DataLad](https://www.datalad.org) which occupies a similar place in the versioning tools ecosystem.
 
 # %% [markdown]
 # ## What is DVC ?
@@ -39,7 +70,8 @@
 # [DVC](https://dvc.org) does not replace or include [Git](https://git-scm.com). You must have [Git](https://git-scm.com) in your system to enable important features such as data versioning and quick experimentation.
 #
 # [DVC](https://dvc.org) is written in Python and can be esily installed on the most common OS (Linux, MacOS, and Windows). We will see how to install [DVC](https://dvc.org) in the next chapter.
-#
+
+# %% [markdown]
 # ### What is the target users of DVC ?
 #
 # [DVC](https://dvc.org) targets people who needs to store and process data files or datasets to produce other data or machine learning models. In other words, anyone willing to:
@@ -49,7 +81,8 @@
 # - understand how datasets and ML artifacts were built in the first place
 # - compare model metrics among experiments
 # - adopt engineering tools and best practices in data science projects
-#
+
+# %% [markdown]
 # ### Versionning data and models
 #
 # #### The dirty way
@@ -68,7 +101,8 @@
 # - the project becomes a mess very fast as people forget what the different names actually mean
 # - the project total weight quickly explodes as everything is copy-pasted
 # - there is no history / lineage, only snapshots in total disorder
-#
+
+# %% [markdown]
 # #### The cleaner way
 #
 # <img src="https://dvc.org/static/39d86590fa8ead1cd1247c883a8cf2c0/cb690/project-versions.png" style="height: 500px;">
